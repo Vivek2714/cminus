e4c418dd6cd8d0584d3aa60ff73919de4243dac9
vivek2714 <vivek2714@gmail.com>
2018-07-05 15:58:15 +0530

    Inserting 5 dynamic fields to form

--- rpdf/class.fields.php
+++ rpdf/class.fields.php
@@ -8,43 +8,89 @@ class cminusFieldsBuilder{
 			return array();
 		}
 
-		//$form = GFAPI::get_form(6);
-
+		$entry = GFAPI::get_entry($targetEntry);
 
 		/*
 		* Static fields
 		*/
 
 		// Agent Details 
-		$agentDetails = GF_Fields::get( 'section' );
-		$agentDetails->label   = 'Agent Details';
+		$agentDetails = GF_Fields::get( "section" );
+		$agentDetails->label   = "Agent Details";
 
 		// Contact Name
-		$contactName = GF_Fields::get( 'name' );
-		$contactName->label   = 'Contact Name';
+		$contactName = GF_Fields::get( "name" );
+		$contactName->label   = "Contact Name";
 
 		// Contact Phone
-		$contactPhone = GF_Fields::get( 'phone' );
-		$contactPhone->label   = 'Contact Phone';
-		$contactPhone->phoneFormat = 'international';
+		$contactPhone = GF_Fields::get( "phone" );
+		$contactPhone->label   = "Contact Phone";
+		$contactPhone->phoneFormat = "international";
 
 		// Contact Email
-		$contactEmail = GF_Fields::get( 'email' );
-		$contactEmail->label   = 'Contact Email';
+		$contactEmail = GF_Fields::get( "email" );
+		$contactEmail->label   = "Contact Email";
 
 		// Company / Branch / Office
-		$company = GF_Fields::get( 'text' );
-		$company->label   = 'Company / Branch / Office';
+		$company = GF_Fields::get( "text" );
+		$company->label   = "Company / Branch / Office";
 
 		// Responses to Brief Criteria
-		$responses = GF_Fields::get( 'section' );
-		$responses->label   = 'Responses to Brief Criteria';
+		$responses = GF_Fields::get( "section" );
+		$responses->label   = "Responses to Brief Criteria";
 
 		// Address of property being put forward
-		$addressOfProperty = GF_Fields::get( 'address' );
-		$addressOfProperty->label   = 'Address of property being put forward';
+		$addressOfProperty = GF_Fields::get( "address" );
+		$addressOfProperty->label   = "Address of property being put forward";
+
+		/*
+		* Dynamic fields
+		*/
+
+		/* Do I want to explore ‘Activity Based Working’ (ABW) or other workplace strategies to suit flexible work styles? */
+		switch ($entry[75]) {
+			case 'Yes':
+				$square_metres	=	$entry[76];
+				break;
+			
+			case 'No':
+				$square_metres	=	$entry[85];
+				break;
+		}
+
+		$fieldFirst = GF_Fields::get( "number" );
+		$fieldFirst->label   = "Our initial estimate for Office Space is for approximately ".$square_metres." m² .";
+		$fieldFirst->numberFormat = "decimal_dot";
+
+		// Missing $form_data['field']['6_name']
 
 
+
+		/* Will my office space needs change over the lease term? */
+		switch ($entry[10]) {
+			case "Yes":
+				$fieldThirdLabel = "We have an expectation that our space needs may grow as much as ".$entry[80]."% over the term of the lease and would prefer to be able to grow within the same building or if possible, even on the same floor.";
+				break;
+			
+			case "No":
+				$fieldThirdLabel = "We have an expectation that our space needs may reduce as much as ".$entry[80]."% over the term of the lease.";
+				break;
+		}
+		$fieldThird = GF_Fields::get( "textarea" );
+		$fieldThird->label   = $fieldThirdLabel;
+
+		#
+		$fieldFourth = GF_Fields::get( "date" );
+		$fieldFourth->label   = "Our current lease is due to expire on ".$entry[87].". Enter when this property will be available:";
+		$fieldFourth->dateType = "datepicker";
+	
+		#
+		$fieldFifth = GF_Fields::get( "number" );
+		$fieldFifth->label   = "We currently have an expected budget of between $ ".$entry[70]." and $ ".$entry[71]." per m². What is the rate for this property?";
+		$fieldFifth->numberFormat = "currency";
+
+
+		// Static fields
 		$fields[] = $agentDetails;
 		$fields[] = $contactName;
 		$fields[] = $contactPhone;
@@ -53,13 +99,18 @@ class cminusFieldsBuilder{
 		$fields[] = $responses;
 		$fields[] = $addressOfProperty;
 
+		// Dynamic fields
+		$fields[] = $fieldFirst;
+		//$fields[] = $field2;
+		$fields[] = $fieldThird;
+		$fields[] = $fieldFourth;
+		$fields[] = $fieldFifth;
 
 
 
 
-		return $fields;
 
-
+		//return $fields;
 
 		echo "<pre>";
 
@@ -68,30 +119,7 @@ class cminusFieldsBuilder{
 		echo "</pre>";
 
 
-
-
-
 		die;
-
-
-		// 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 		// HTML Field
 		$htmlField = GF_Fields::get( 'html' );
